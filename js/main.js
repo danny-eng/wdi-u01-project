50dd0c51800a5306760498f4775c89da09a270dd
Danny Eng <dannyeng94@gmail.com>
2017-10-09 18:56:16 -0400

    Made trails.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 
 
+
+// EXPLOSION LOGIC
+
+
+
 // Listen to what key has been pressed!
 document.addEventListener("keydown", inputListener);
 
 // This handles all the key controls.
 
 // Z, X, C handles firing from turrets A, B, C respectively.
+// side note: A = 90vh, 10vw
+//            B = 90vh, 50vw
+//            C = 90vh, 90vw
 
 function inputListener(event){
     let e = event;
     if (e.code === "KeyZ"){
-        console.log("Fire A!");
         fireMissile(e.code);
     }
     if (e.code === "KeyX"){
-        console.log("Fire B!");
         fireMissile(e.code);
     }
     if (e.code === "KeyC"){
-        console.log("Fire C!");
         fireMissile(e.code);
     }
 }
@@ -45,6 +50,41 @@ function cursorTracker(event){
 // The explosion.
 function fireMissile(key){
 
+    let missileDraw = document.createElement("div");
+    missileDraw.setAttribute("class", "fMissile");
+    document.body.appendChild(missileDraw);
+
+    let x_origin = document.documentElement.clientWidth;
+    let y_origin = document.documentElement.clientHeight * 0.9;
+
+      if (key === "KeyZ"){
+        x_origin = x_origin * 0.1;
+      }
+      if (key === "KeyX"){
+        x_origin = x_origin * 0.5;
+      }
+      if (key === "KeyC"){
+        x_origin = x_origin * 0.9;
+      }
+
+    // origin of trail
+    missileDraw.style.top = y_origin + 'px';
+    missileDraw.style.left = x_origin + 'px';
+
+    // imagine this is a triangle
+    // trail is the hypotenuse
+    // opposite is y-difference
+    // adjacent is x-difference
+    let opposite = mouseY - y_origin;
+    let adjacent = mouseX - x_origin;
+    let hypotenuse = Math.sqrt(Math.pow(opposite, 2) + Math.pow(adjacent, 2));
+    let angle = Math.atan2(opposite, adjacent) * 180 / Math.PI;
+    missileDraw.style.transform = `rotate(${angle}deg)`;
+    missileDraw.style.width = hypotenuse + `px`;
+    window.setTimeout(function(){
+      missileDraw.remove();
+    }, 1000);
+
     // Creates a div that uses the explosion class.
     let explosion = document.createElement("div");
     explosion.setAttribute("class", "explosion");
@@ -62,3 +102,15 @@ function fireMissile(key){
         explosion.remove();
     }, 1000);
 }
+
+
+
+
+
+// FALLING MISSILES LOGIC
+
+
+
+
+
+
